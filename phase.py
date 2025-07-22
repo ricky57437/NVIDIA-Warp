52cb7294e0632025843f38461cc979dd3e310c91
ricky57437 <ricky57437@gmail.com>
2025-07-22 10:32:45 -0500

    Create phase.py

---
+++ phase.py
@@ -0,0 +1,177 @@
+import os
+
+import numpy as np
+
+import warp as wp
+import warp.examples
+import warp.sim
+import warp.sim.render
+import math
+import random
+
+from pxr import Gf
+
+
+class Example:
+    def __init__(self, stage_path="phase.usd"):
+        #initial settings, setup such as the fps and radius of the particles.
+        fps = 100
+        self.frame_dt = 1.0 / fps
+
+        self.sim_substeps = 10000
+        self.sim_dt = self.frame_dt / self.sim_substeps
+        self.sim_time = 0.0
+
+        self.radius = 0.05
+
+        #initialize the model builder
+        builder = wp.sim.ModelBuilder()
+        builder.default_particle_radius = self.radius
+        builder.default_particle_color = Gf.Vec4f(0.0, 1.0, 0.0, 1.0)
+        
+        #make the particle grid, dimensions given and the cell sizes.
+        builder.add_particle_grid(
+            #dimensions of the rectangular grid
+            dim_x=80, #how many can fit in 29 #58
+            dim_y=60, #14
+            dim_z=80, #58
+            cell_x=self.radius * 2.0,
+            cell_y=self.radius * 2.0,
+            cell_z=self.radius * 2.0,
+            #where the center is?
+            pos=wp.vec3(-4.5, 35, -4.5), 
+            #rotation
+            rot=wp.quat_identity(),
+            #initial velocity of the particles
+            vel=wp.vec3(0.0, 0.0, 0.0),
+            mass=20,
+            jitter=self.radius * 0.1,
+        )
+
+        # for i in range(len(builder.particle_q)):
+        #     builder.particle_radius[i] = random.choice([.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.25])
+        #     #can change other things from builder (model.py lines 1183-1188)
+        #     #MAYBE THIS HELPS WITH HOLLOW?
+        #     if builder.particle_radius[i] == 0.1:
+        #         builder.particle_mass[i] = .1
+
+        self.ke = 1e5
+        self.kd = 1e4
+
+        #big slant
+        builder.add_shape_box(
+            pos=wp.vec3(0,18.5,0),
+            hx=5,
+            hy=1.5,
+            hz=5,
+            density=1,
+            is_solid=True,
+            body=-1,
+            is_visible=True,
+            mu=0.0,
+            ke=self.ke,
+            kd=self.kd
+        )
+
+        #finaliszes the builder ive been creating this whole time
+        self.model = builder.finalize()
+        #physics for the particles themselves when bumping into each other
+        self.model.particle_kf = 50.0
+        #resistance to sliding btwn two particles
+        
+        self.model.particle_ke = 100.0
+        self.model.particle_kd = 10.0
+        self.model.particle_mu = 0.1
+        #mu: particle friction coefficient GAME CHANGER
+        
+        
+        self.model.particke_cohesion = 0.0
+        #cohesion: attraction between molecules of the same substance
+        self.model.particle_adhesion = 0.0
+        #adhesion: attraction between molecules of different substances
+
+        self.model.gravity = wp.vec3(0.0, -18.0, 0.0)
+        
+
+        """
+        self.model.soft_contact_kd = 100.0
+        self.model.soft_contact_kf *= 2.0
+        """
+
+        #creating 2 simulation states
+        self.state_0 = self.model.state()
+        self.state_1 = self.model.state()
+
+        #creates the integrator, which solves how particles move and interact over time.
+        self.integrator = wp.sim.SemiImplicitIntegrator()
+
+        #sets up usd output rendering to visulize the simulation.
+        if stage_path:
+            self.renderer = wp.sim.render.SimRenderer(self.model, stage_path, scaling=20.0)
+        else:
+            self.renderer = None
+
+        #tells to use CUDA graph for performance if available.
+        self.use_cuda_graph = wp.get_device().is_cuda
+        if self.use_cuda_graph:
+            with wp.ScopedCapture() as capture:
+                self.simulate()
+            self.graph = capture.graph
+
+#Should be using this in all simulations?
+    def simulate(self):
+        for _ in range(self.sim_substeps):
+            self.state_0.clear_forces()
+            wp.sim.collide(self.model, self.state_0)
+            self.integrator.simulate(self.model, self.state_0, self.state_1, self.sim_dt)
+
+            # swap states
+            (self.state_0, self.state_1) = (self.state_1, self.state_0)
+
+    def step(self):
+        with wp.ScopedTimer("step"):
+            self.model.particle_grid.build(self.state_0.particle_q, self.radius * 2.0)
+            if self.use_cuda_graph:
+                wp.capture_launch(self.graph)
+            else:
+                self.simulate()
+
+        self.sim_time += self.frame_dt
+
+    def render(self):
+        if self.renderer is None:
+            return
+
+        with wp.ScopedTimer("render"):
+            self.renderer.begin_frame(self.sim_time)
+            # Render particles
+            self.renderer.render(self.state_0)
+            self.renderer.end_frame()
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--device", type=str, default=None, help="Override the default Warp device.")
+    parser.add_argument(
+        "--stage_path",
+        type=lambda x: None if x == "None" else str(x),
+        default="phase.usd",
+        help="Path to the output USD file.",
+    )
+    parser.add_argument("--num_frames", type=int, default=200, help="Total number of frames.")
+
+    args = parser.parse_known_args()[0]
+
+    with wp.ScopedDevice(args.device):
+        example = Example(stage_path=args.stage_path)
+
+        for _ in range(args.num_frames):
+            example.step()
+            example.render()
+
+        if example.renderer:
+            example.renderer.save()
+    
+
+    
